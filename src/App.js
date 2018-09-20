53b807784d00bb4f6fa18d3b858cf8c5db8d1159
Robert Cereghini <rcereghini@Roberts-MacBook-Pro.local>
2018-09-20 12:51:34 -0700

    Added form

--- src/App.js
+++ src/App.js
@@ -6,9 +6,23 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-
-          <img src={logo} className="App-logo" alt="logo" />
-          
+        <div className="formWrap">
+          <h2>Freedom is Here</h2>
+          <form className="landingForm" name="contact" method="POST" netlify>
+            <p>
+              <label>Name: <input type="text" name="name" /></label>
+            </p>
+            <p>
+              <label>Email: <input type="email" name="email" /></label>
+            </p>
+            <p>
+              <label>Message: <textarea name="message"></textarea></label>
+            </p>
+            <p>
+              <button type="submit">Send</button>
+            </p>
+          </form>
+        </div>
       </div>
     );
   }

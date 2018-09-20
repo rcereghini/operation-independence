4f3b678a458fa71e270e1701d692759aca53d44c
Robert Cereghini <rcereghini@cleartitleaz.local>
2018-09-20 09:31:44 -0700

    Added form

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
-import React, { Component } from 'react';
-import logo from './images/operationIndependence.jpg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./images/operationIndependence.jpg";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
-          <img src={logo} className="App-logo" alt="logo" />
-          
+        <h2>Looking for Freedom?</h2>
+        <form name="contact" method="POST" netlify>
+          <p>
+            <label>
+              Your Name: <input type="text" name="name" />
+            </label>
+          </p>
+          <p>
+            <label>
+              Your Email: <input type="email" name="email" />
+            </label>
+          </p>
+          <p>
+            <label>
+              Message: <textarea name="message" />
+            </label>
+          </p>
+          <p>
+            <button type="submit">Send</button>
+          </p>
+        </form>
       </div>
     );
   }

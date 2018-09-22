052c1a3df2508ae5c2c1f1b63037586cfc9fad1e
Robert Cereghini <rcereghini@Roberts-MacBook-Pro.local>
2018-09-22 13:04:18 -0700

    Added the home page form

--- src/App.js
+++ src/App.js
@@ -8,23 +8,27 @@ class App extends Component {
       <div className="App">
         <img className="headerLogo" src={logo}/>
         <div className="formWrap">
-          <h2>Freedom is Here</h2>
-          <form className="landingForm" name="contact" method="POST" netlify>
-            <p>
-              <label>Name: <input type="text" name="name" /></label>
-            </p>
-            <p>
-              <label>Email: <input type="email" name="email" /></label>
-            </p>
-            <p>
-              <label>Message: <textarea name="message"></textarea></label>
-            </p>
-            <p>
-              <button type="submit">Send</button>
-            </p>
+          <form name="homeScreen" netlify>
+            <p className="homeFormText">Please provide your information and an associate will be in contact with you shortly...</p><br/>
+            <div class="form-group">
+              <label for="exampleInputEmail1">Name</label>
+              <input type="email" className="form-control" id="exampleInputEmail2" placeholder="Enter Name"/>
+            </div>
+            <div class="form-group">
+              <label for="exampleInputEmail1">Email address</label>
+              <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
+            </div>
+            <div class="form-group">
+              <label for="exampleInputEmail1">Phone Number</label>
+              <input type="email" className="form-control" id="exampleInputEmail2" placeholder="Phone Number"/>
+            </div>
+            <div class="form-group form-check">
+              <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
+              <label class="form-check-label" for="exampleCheck1">Subscribe to Newsletter</label>
+            </div>
+            <button type="submit" className="btn btn-primary formSubmitButton">Submit</button>
           </form>
         </div>
-
       </div>
     );
   }

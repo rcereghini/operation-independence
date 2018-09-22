da1a652a7f9d86c1fa860c45e0903564c7f5be8a
Robert Cereghini <rcereghini@Roberts-MacBook-Pro.local>
2018-09-22 12:12:14 -0700

    Added logo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
-import logo from "./images/operationIndependence.jpg";
+import logo from "./images/OpinAdsLogo.png";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <img className="headerLogo" src={logo}/>
         <div className="formWrap">
           <h2>Freedom is Here</h2>
           <form className="landingForm" name="contact" method="POST" netlify>

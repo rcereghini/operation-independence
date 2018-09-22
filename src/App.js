b7f14ff5af3079309eb5f57cdc8b3a7e1a4b8f45
Robert Cereghini <rcereghini@Roberts-MacBook-Pro.local>
2018-09-22 13:43:55 -0700

    Added the sliders and turned the form into a component.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import Navigation from './components/Navigation.js'
+import Carousel from './components/Carousel.js'
+import HomeForm from './components/HomeForm.js'
 import logo from "./images/OpinAdsLogo.png";
 import "./App.css";
 
@@ -8,29 +10,8 @@ class App extends Component {
     return (
       <div className="App">
       <Navigation/>
-        <img className="headerLogo" src={logo}/>
-        <div className="formWrap">
-          <form name="homeScreen" netlify>
-            <p className="homeFormText">Please provide your information and an associate will be in contact with you shortly...</p><br/>
-            <div class="form-group">
-              <label for="exampleInputEmail1">Name</label>
-              <input type="email" className="form-control" id="exampleInputEmail2" placeholder="Enter Name"/>
-            </div>
-            <div class="form-group">
-              <label for="exampleInputEmail1">Email address</label>
-              <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
-            </div>
-            <div class="form-group">
-              <label for="exampleInputEmail1">Phone Number</label>
-              <input type="email" className="form-control" id="exampleInputEmail2" placeholder="Phone Number"/>
-            </div>
-            <div class="form-group form-check">
-              <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
-              <label class="form-check-label" for="exampleCheck1">Subscribe to Newsletter</label>
-            </div>
-            <button type="submit" className="btn btn-primary formSubmitButton">Submit</button>
-          </form>
-        </div>
+      <Carousel/>
+      <HomeForm/>
       </div>
     );
   }
---
+++ src/components/HomeForm.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import '../styles/homeForm.css'
+
+function HomeForm(props){
+    return(
+      <div className="formWrap">
+        <form name="homeScreen" netlify>
+          <p className="homeFormText">Please provide your information and an associate will be in contact with you shortly...</p><br/>
+          <div class="form-group">
+            <label for="exampleInputEmail1">Name</label>
+            <input type="email" className="form-control" id="exampleInputEmail2" placeholder="Enter Name"/>
+          </div>
+          <div class="form-group">
+            <label for="exampleInputEmail1">Email address</label>
+            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
+          </div>
+          <div class="form-group">
+            <label for="exampleInputEmail1">Phone Number</label>
+            <input type="email" className="form-control" id="exampleInputEmail2" placeholder="Phone Number"/>
+          </div>
+          <div class="form-group form-check">
+            <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
+            <label class="form-check-label" for="exampleCheck1">Subscribe to Newsletter</label>
+          </div>
+          <button type="submit" className="btn btn-primary formSubmitButton">Submit</button>
+        </form>
+      </div>
+    )
+}
+
+export default HomeForm

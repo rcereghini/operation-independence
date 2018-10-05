5fa45fc10298099c4dd61966fcf689f1fe95b103
Robert Cereghini <rcereghini@Roberts-MacBook-Pro.local>
2018-10-04 23:59:45 -0700

    Fully responsive homepage

--- src/components/HomeForm.js
+++ src/components/HomeForm.js
@@ -9,23 +9,24 @@ function HomeForm(props){
             <h2>Ready to Learn More?</h2>
             <p className="homeFormText">Please provide your information, and an associate will be in contact with you shortly...</p><br/>
           </div>
-          <div class="form-group">
+          <div className="form-group">
             <label for="exampleInputEmail1">Name</label>
             <input type="email" className="form-control" id="exampleInputEmail2" placeholder="Enter Name"/>
           </div>
-          <div class="form-group">
+          <div className="form-group">
             <label for="exampleInputEmail1">Email address</label>
             <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
           </div>
-          <div class="form-group">
+          <div className="form-group">
             <label for="exampleInputEmail1">Phone Number</label>
             <input type="email" className="form-control" id="exampleInputEmail2" placeholder="Phone Number"/>
           </div>
-          <div class="form-group form-check">
+          <div className="form-group form-check">
             <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
-            <label class="form-check-label" for="exampleCheck1">Subscribe to Newsletter</label>
-          </div>
+            <label className="form-check-label" for="exampleCheck1">Subscribe to Newsletter</label>
+          </div><div className="formSubmitButtonWrap">
           <button type="submit" className="btn btn-primary formSubmitButton">Submit</button>
+        </div>
         </form>
       </div>
     )
